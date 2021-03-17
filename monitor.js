6ad00f469710a2fd38518eb0cd13b3c1149c2fca
TheMezStrikes <TheMezStrikes@gmail.com>
2021-03-17 18:34:44 +0100

    Clear out monitor

--- monitor.js
+++ monitor.js
@@ -1,11 +1,3 @@
 
 exports.phrases = [];
-exports.monitor = function(user, message) {
-    let p = toId(user) + toId(message);
-    for (let i in this.phrases) {
-        if (p.match(this.phrases[i])) {
-            if (!Users['unleashourpassion']) return;
-            return Users['unleashourpassion'].send(user + ": " + message);
-        }
-    }
-}
+exports.monitor = function(user, message) {}
