579830cbd0c71abc9b4d4a72f24eead691259b3e
TheMezStrikes <TheMezStrikes@gmail.com>
2021-03-17 18:37:03 +0100

    Add avatar to default config

--- config-example.js
+++ config-example.js
@@ -5,19 +5,21 @@
 exports.username = "";
 exports.password = "";
 
+exports.avatar = 167;
+
 /*
  * Other
  */
- 
+
 exports.rooms = [];
 exports.char = '.';
 exports.devs = [];
 
-exports.git = 'https://github.com/TheMezStrikes/bot-base/'
+exports.git = 'https://github.com/TheMezStrikes/bot-base/';
 /*
  * Mail
  */
- 
+
 exports.mail = {
     'inboxSize': 5,
     'bans': []
